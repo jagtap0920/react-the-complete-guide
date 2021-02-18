ef8556b83d54965f1c90a96c2c5b5d529f8cf5e8
Tushar Jagtap <t.jagtap@easternenterprise.com>
2021-02-18 10:48:38 +0530

    [TASK] Learn state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 import React, {Component} from 'react';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>This is React App</h1>
-      <Person name="Tushar" age="29" />
-      <Person name="Anshdeep" age="27" >He is amazing guy!!</Person>
-    </div>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      persons: [
+        {name: 'Tushar', age:29},
+        {name: 'Anshdeep', age:28},
+      ]
+    }
+  }
 
-      //React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'This is react JSX'))
+  render() {
+    return (
+      <div className="App">
+        <h1>This is React App</h1>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >He is amazing guy!!</Person>
+      </div>
 
-  );
+        //React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'This is react JSX'))
+
+    );
+  }
 }
 
 export default App;
